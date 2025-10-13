0a9943e4e5a19373c25b25d723890e55ef107c93
roxblnfk <roxblnfk@ya.ru>
2025-10-13 13:02:33 +0400

    feat: Init configs

---
+++ src/Config/ApplicationConfig.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Testo\Config;
+
+/**
+ * Test Suite configuration.
+ */
+final class ApplicationConfig
+{
+    public function __construct(
+        /**
+         * Source code location.
+         */
+        public readonly ?FinderConfig $src = null,
+
+        /**
+         * Specify one or more Test Suites to be executed.
+         *
+         * @var non-empty-list<SuiteConfig>
+         */
+        public readonly array $suites = [
+            new SuiteConfig(
+                name: 'default',
+                location: new FinderConfig('tests'),
+            ),
+        ],
+    ) {
+        $suites === [] and throw new \InvalidArgumentException('At least one test suite must be defined.');
+    }
+}
---
+++ src/Config/FinderConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Testo\Config;
+
+/**
+ * File system scope configuration.
+ */
+final class FinderConfig
+{
+    public function __construct(
+        string|\Stringable $rootPath,
+    ) {}
+}
