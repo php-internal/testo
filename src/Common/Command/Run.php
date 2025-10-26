6396c2deff93751f04f07ff21b4d311211fa7437
Dmitrii Derepko <xepozz@list.ru>
2025-10-26 15:40:18 +0400

    feat: add `--teamcity` option (#17)

--- src/Common/Command/Run.php
+++ src/Common/Command/Run.php
@@ -7,6 +7,7 @@ namespace Testo\Common\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Testo\Render\StdoutRenderer;
 use Testo\Render\TeamcityInterceptor;
@@ -17,12 +18,21 @@ use Testo\Render\TerminalInterceptor;
 )]
 final class Run extends Base
 {
+    public function configure(): void
+    {
+        parent::configure();
+        $this->addOption('teamcity', null, InputOption::VALUE_NONE);
+    }
+
     public function __invoke(
-        InputInterface $input,
+        InputInterface  $input,
         OutputInterface $output,
     ): int {
-        // $this->container->bind(StdoutRenderer::class, TeamcityInterceptor::class);
-        $this->container->bind(StdoutRenderer::class, TerminalInterceptor::class);
+        if ($input->getOption('teamcity')) {
+            $this->container->bind(StdoutRenderer::class, TeamcityInterceptor::class);
+        } else {
+            $this->container->bind(StdoutRenderer::class, TerminalInterceptor::class);
+        }
 
         $result = $this->application->run();
 
